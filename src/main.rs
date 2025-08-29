6f9ddf6ad0eecef7b7785015339072061a29752c
Luis <317201273@pcpuma.acatlan.unam.mx>
2025-08-28 23:22:27 -0600

    deep purple

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use macroquad::prelude::*;
 #[macroquad::main("Mai guemu")]
 async fn main() {
     loop{
-        clear_background(DARKPURPLE);
+        clear_background(DARKBLUE);
         next_frame().await
     }
 
